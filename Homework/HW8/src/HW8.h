43aa42eabf1fd5a082ff3b23cca1b9a8f1fc9ffc
fantasticgold <fantasticgold@foxmail.com>
2019-06-03 02:15:58 +0800

    HW8.h

--- Homework/HW8/src/HW8.h
+++ Homework/HW8/src/HW8.h
@@ -1,5 +1,26 @@
-#pragma once
 #include "global.h"
+#include "Shader.h"
 
+const int N = 50;
 
-#pragma once
+const int SIZE_POINT = 10;
+const int SIZE_LINE = 1;
+const int SIZE_BEZIER = 5;
+
+struct Point {
+  double x;
+  double y;
+  Point(double _x = 0, double _y = 0) : x(_x), y(_y) {
+
+  }
+  Point operator+(const Point p2) const {
+    return Point(this->x + p2.x, this->y + p2.y);
+  }
+  Point operator*(double d) const {
+    return Point(this->x * d, this->y * d);
+  }
+};
+
+void initBezier();
+void renderBezier(unsigned int VAO, unsigned int VBO, vector<Point>& points);
+void renderAnimation(unsigned int VAO, unsigned int VBO, vector<Point>& points, float time);
